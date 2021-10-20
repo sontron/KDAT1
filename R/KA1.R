b1259c63bc01d24390dbd581efddae58b2d5d301
sontron <sontron@foxmail.com>
2021-10-20 09:53:35 +0800

    revise

--- R/KA1.R
+++ R/KA1.R
@@ -7,11 +7,11 @@
 #'
 #' @export
 KT1 <- function(wd=paste0(getwd(),'/'),Encod='utf8-8',...) {
-  sd=paste(path.package('KDAT1'),'/app/',sep='')
-
-    readLines(paste0(sd,'appCN.R'),encoding = Encod)->app
-    c(paste("wd=",paste('\"',wd,'\"',sep=''),sep=''),app)->appNew
-    writeLines(appNew,paste0(sd,'app.R'))
+  # sd=paste(path.package('KDAT1'),'/app/',sep='')
+  # 
+  #   readLines(paste0(sd,'appCN.R'),encoding = Encod)->app
+  #   c(paste("wd=",paste('\"',wd,'\"',sep=''),sep=''),app)->appNew
+  #   writeLines(appNew,paste0(sd,'app.R'))
   library(shiny)
   runApp(system.file("app", package = "KDAT1"), ...)
 }
